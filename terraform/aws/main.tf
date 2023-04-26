fe83230058f468e7a654eaea25a91ae6a77ac062
donghli <donghli@tencent.com>
2023-04-26 16:26:51 +0800

    aws cluster tf

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,15 +1,10 @@
-# use ubuntu 20 AMI for EC2 instance
-data "aws_ami" "ubuntu" {
-    most_recent = true
-    filter {
-        name = "name"
-        values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.0"
     }
-    filter {
-        name = "virtualization-type"
-        values = ["hvm"]
-    }
-    owners = ["099720109477"] # Canonical
+  }
 }
 
 # provision to ap-southeast-2 region
@@ -17,12 +12,75 @@ provider "aws" {
     region = "ap-southeast-2"
 }
 
-resource "aws_instance" "app_server" {
-  ami = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  key_name = "app-ssh-key"
+resource "aws_ecr_repository" "ai_ci_ecr_repo" {
+  name = "ai-ci-ecr-repo"
+}
 
-  tags = {
-    Name = var.ec2_name
+resource "aws_ecs_cluster" "ai_ci_cluster" {
+  name = "ai-ci-cluster"
+}
+
+resource "aws_ecs_task_definition" "ai_ci_task" {
+  family = "ai-ci-task"
+  container_definitions = jsonencode([
+    {
+      name = "${var.server_bin_name}"
+      image = "${aws_ecr_repository.ai_ci_ecr_repo.repository_url}"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort = 8080
+        }
+      ]
+      memory = 512
+      cpu = 256
+    }
+  ])
+  requires_compatibilities = [ "FARGATE" ]
+  network_mode = "awsvpc"
+  memory = 512
+  cpu = 256
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs_task_execution_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
   }
+}
+
+resource "aws_ecs_service" "ai_ci_service" {
+  name = "ai-ci-service"
+  cluster = aws_ecs_cluster.ai_ci_cluster.id
+  task_definition = aws_ecs_task_definition.ai_ci_task.arn
+  launch_type = "FARGATE"
+  desired_count = 3
+
+  network_configuration {
+    subnets = [ aws_default_subnet.subnet_a.id, aws_default_subnet.subnet_b.id, aws_default_subnet.subnet_c.id ]
+    assign_public_ip = true
+  }
+}
+
+resource "aws_default_subnet" "subnet_a" {
+  availability_zone = "ap-southeast-2a"
+}
+
+resource "aws_default_subnet" "subnet_b" {
+  availability_zone = "ap-southeast-2b"
+}
+
+resource "aws_default_subnet" "subnet_c" {
+  availability_zone = "ap-southeast-2c"
 }
--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -2,3 +2,8 @@ variable "ec2_name" {
   type = string
   default = "SimpleAlbumServer"
 }
+
+variable "server_bin_name" {
+  type = string
+  default = "albumsvr"
+}
